43f5417372957ce0b7f01305dd418d6c4b1e77e5
Will Figueiredo <will@wfigueiredo.com>
2018-08-04 21:06:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,41 +2,33 @@
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
-  kittens.unshift(name);
-  name = 'Ralph';
-  return kittens;
-}
-
-function destructivelyPrependKitten(name) {
-  kittens.shift(name);
-  name = 'Bob';
-  return kittens;
+  return kittens.push(name);
 }
 
 function destructivelyRemoveLastKitten() {
-  kittens.pop();
-  return kittens;
+  return kittens.pop()
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
 }
 
 function destructivelyRemoveFirstKitten() {
-  
+  return kittens.shift();
 }
 
 function appendKitten(name) {
-  
+  return [...kittens, name];
 }
 
 function prependKitten(name) {
-  
+  return [name, ...kittens];
 }
 
 function removeLastKitten() {
-  
+  return kittens.slice(0, kittens.length - 1);
 }
 
 function removeFirstKitten() {
-  
+  return kittens.slice(1);
 }
-
-
-
